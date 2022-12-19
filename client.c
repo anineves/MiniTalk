57ea8d12dbce801b6a5082fadeec408c01ff69a0
Ana Isabel Neves <85195243+anineves@users.noreply.github.com>
2022-12-19 17:32:20 +0000

    Create client.c

---
+++ client.c
@@ -0,0 +1,37 @@
+void	send_msg(int pid, char *msg, size_t len_msg)
+{
+	int		nbit;
+	size_t	i;
+
+	i = 0;
+	while (i <= len_msg)
+	{
+		nbit = 0;
+		while (shift < 7)
+		{
+			if ((msg[i] >> nbit) & 1)
+				kill(pid, SIGUSR2);
+			else
+				kill(pid, SIGUSR1);
+			nbit++;
+			usleep(300);
+		}
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int		pid;
+	char	*msg;
+  size_t len_msg;
+
+
+	if (argc == 3)
+	{
+		pid = ft_atoi(argv[1]);
+		msg = argv[2];
+    len_msg = ft_strlen(msg)
+		send_msg(pid, msg, len_msg);
+	}
+}
