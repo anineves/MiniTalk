a86f33a9b07cd66035934bc07a68e18d2acdfefd
Ana Isabel Neves <85195243+anineves@users.noreply.github.com>
2022-12-20 11:43:32 +0000

    Update client.c

--- client.c
+++ client.c
@@ -20,6 +20,18 @@ void	send_msg(int pid, char *msg, size_t len_msg)
 	}
 }
 
+void	config_signals(void)
+{
+	struct sigaction	sa_nsigact;
+
+	sa_nsigact.sa_flags = SA_SIGINFO;
+	if (sigaction(SIGUSR1, &sa_nsigact, NULL) == -1)
+		handle_errors("Failed SIGUSR1");
+	if (sigaction(SIGUSR2, &sa_nsigact, NULL) == -1)
+		handle_errors("Failed SIGUSR2");
+}
+
+
 int	main(int argc, char **argv)
 {
 	int		pid_server;
@@ -31,7 +43,12 @@ int	main(int argc, char **argv)
 	{
 		pid_server = ft_atoi(argv[1]);
 		msg = argv[2];
+		config_signals();
 		len_msg = ft_strlen(msg);
 		send_msg(pid, msg, len_msg);
+	}
+	else{
+		ft_printf("Error");
+		
 	}
 }
