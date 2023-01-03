850d68a46e5ffe56e6d8d23a2f79c55fe5ce7e09
Ana Isabel Neves <85195243+anineves@users.noreply.github.com>
2023-01-03 11:53:03 +0000

    Update server.c

--- server.c
+++ server.c
@@ -1,43 +1,69 @@
-void	handle_signals(int signum, siginfo_t *info, void *ucontent)
+#include "minitalk.h"
+#include <stdio.h>
+
+void	handle_signals(int signal)
 {
-	static  int nbits;
-	static  char	c;
-  
-  n=0;
-  
-	if (nbits < 0)
-		bit_itr = 8;
+	static int				i = 0;
+	static unsigned char	c = 0;
+
+
+	if(signal == SIGUSR1)
+		c |= (1 << i);
+	i++;
+	if (i == 8)
+	{
+		ft_putchar_fd(c, 1);
+		c = 0;
+		i = 0;
+	}
+}
+
+
+/* void	handle_signals(int signum, siginfo_t *info, void *ucontent)
+{
+	static int				bit_itr = -1;
+	static unsigned char	c;
+
+	(void)ucontent;
+	if (bit_itr < 0)
+		bit_itr = 7;
 	if (signum == SIGUSR1)
 		c |= (1 << bit_itr);
-	nbits--;
-	if (nbits < 0 && c)
+	bit_itr--;
+	if (bit_itr < 0 && c)
 	{
+		ft_putchar_fd(c, 1);
 		c = 0;
 		if (kill(info->si_pid, SIGUSR2) == -1)
-			handle_errors("failed to send SIGUSR2");
+			printf("Failed SIGUSR2");
 		return ;
 	}
 	if (kill(info->si_pid, SIGUSR1) == -1)
-		handle_errors("Failed to send SIGUSR1");
+		printf("Failed SIGUSR1");
 }
+*/
 
 void	config_signals(void)
 {
 	struct sigaction	sa_sigact;
 
-	sa_sigact.sa_sigaction = &handle_signals;
+	sa_sigact.sa_handler = &handle_signals;
 	sa_sigact.sa_flags = SA_SIGINFO;
 	if (sigaction(SIGUSR1, &sa_sigact, NULL) == -1)
-		handle_errors("SIGUSR1");
+		printf("Error SIGUSR1");
 	if (sigaction(SIGUSR2, &sa_sigact, NULL) == -1)
-		handle_errors("Failed to change SIGUS");
+		printf("Error SIGUR2");
 }
 
 int	main(void)
 {
 	pid_t	pid;
+	
 
 	pid = getpid();
-	ft_printf("PID = %d\n\", pid);
+	printf("PID = %d\n", pid);
 	config_signals();
+	while (1)
+		pause();
+	return (0);
 }
