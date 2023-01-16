0702ba096126f6cff9f2ee05596c8e1ad7b604f3
Ana Isabel Neves <85195243+anineves@users.noreply.github.com>
2023-01-16 17:32:24 +0000

    Update server.c

--- server.c
+++ server.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asousa-n <asousa-n@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/21 09:45:51 by asousa-n          #+#    #+#             */
+/*   Updated: 2022/12/21 10:25:34 by asousa-n         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minitalk.h"
 #include <stdio.h>
 
@@ -6,8 +18,7 @@ void	handler_signals(int signal)
 	static int				i = 0;
 	static unsigned char	c = 0;
 
-
-	if(signal == SIGUSR1)
+	if (signal == SIGUSR1)
 		c |= (1 << i);
 	i++;
 	if (i == 8)
@@ -17,31 +28,6 @@ void	handler_signals(int signal)
 		i = 0;
 	}
 }
-
-
-/*void	handler_signals(int signal, siginfo_t *info, void *ucontent)
-{
-	static int				i = 0;
-	static unsigned char	c = 0;
-
-	(void)ucontent;
-	if (i < 0)
-		i = 7;
-	if(signal == SIGUSR1)
-		c |= (1 << i);
-	i++;
-	if (i == 8)
-	{
-		ft_putchar_fd(c, 1);
-		c = 0;
-		i = 0;
-		if (kill(info->si_pid, SIGUSR2) == -1)
-			printf("Failed SIGUSR2");
-	}
-	if (kill(info->si_pid, SIGUSR1) == -1)
-		printf("Failed SIGUSR1");
-}
-*/
 
 void	config_signals(void)
 {
@@ -50,15 +36,14 @@ void	config_signals(void)
 	sa_sigact.sa_handler = &handler_signals;
 	sa_sigact.sa_flags = SA_SIGINFO;
 	if (sigaction(SIGUSR1, &sa_sigact, NULL) == -1)
-		printf("Error SIGUSR1");
+		ft_printf("Error SIGUSR1");
 	if (sigaction(SIGUSR2, &sa_sigact, NULL) == -1)
-		printf("Error SIGUR2");
+		ft_printf("Error SIGUR2");
 }
 
 int	main(void)
 {
 	pid_t	pid;
-	
 
 	pid = getpid();
 	printf("PID = %d\n", pid);
